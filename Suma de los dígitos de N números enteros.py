f6462f2e01412b13687c0b22a3fb34a9ca023bc2
Ruben <guaman_ruben@hotmail.com>
2018-07-13 21:00:35 -0500

    mi primer programa en git

---
+++ Suma de los dígitos de N números enteros.py
@@ -0,0 +1,14 @@
+def sumad(num):
+    s=0 #suma de los digitos
+    while num>0:
+        s=s+num%10
+        num=num//10
+        return s
+n=int(input("cantidad de numeros:"))
+sumat = 0#sumatotal
+while n>0:
+        num = int(input("Numero:"))
+        sumat=sumat+sumad(num)
+        n=n-1
+print (sumat)
+
